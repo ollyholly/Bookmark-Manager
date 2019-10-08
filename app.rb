86ab21fe63b3dbebb098d3db0c1b2d740ba36d6b
Olly Holovchenko <oleh.holo@gmail.com>
2019-10-08 22:16:36 +0100

    Finish CRUD features for the app

--- app.rb
+++ app.rb
@@ -2,7 +2,8 @@ require 'sinatra/base'
 require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
-  # ... app code here ...
+  enable :sessions, :method_override
+
   get '/' do
     'Hello world'
   end
@@ -19,6 +20,24 @@ class BookmarkManager < Sinatra::Base
 
   get '/bookmarks/new' do
     erb :'bookmarks/new'
+  end
+
+  delete '/bookmarks/:id' do
+    Bookmark.delete(id: params[:id])
+    redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do
+    # @bookmark_id = params[:id]
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :'bookmarks/edit'
+  end
+  
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], url: params[:url], title: params[:title])
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("UPDATE bookmarks SET url = '#{params[:url]}', title = '#{params[:title]}' WHERE id = '#{params[:id]}'")
+    redirect :'/bookmarks'
   end
 
   # start the server if ruby file executed directly
