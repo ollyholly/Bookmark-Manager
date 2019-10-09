64f161be511841091e1d055e5a6185773a8b9788
Olly Holovchenko <oleh.holo@gmail.com>
2019-10-09 08:46:16 +0100

    Extract Database Setup object

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,23 @@
-ENV['ENVIRONMENT'] = 'test'
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
-require 'capybara'
-require 'capybara/rspec'
-require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
-require_relative './setup_test_database'
-
-Capybara.app = BookmarkManager
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   SimpleCov::Formatter::HTMLFormatter
   ])
 SimpleCov.start
+
+ENV['ENVIRONMENT'] = 'test'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require_relative './setup_test_database'
+
+
+
+Capybara.app = BookmarkManager
+
 
 RSpec.configure do |config|
 
